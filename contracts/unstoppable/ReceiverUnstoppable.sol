400076511bc21873a735a006da80aea420746f39
鄭閔鴻 <tommy@zhengminhongdeMacBook-Pro.local>
2022-12-03 18:42:57 +0800

    chore: Add comments for unstoppable

--- contracts/unstoppable/ReceiverUnstoppable.sol
+++ contracts/unstoppable/ReceiverUnstoppable.sol
@@ -14,18 +14,23 @@ contract ReceiverUnstoppable {
     UnstoppableLender private immutable pool;
     address private immutable owner;
 
+    // 部署合約時就指定了所要借款的池子合約
     constructor(address poolAddress) {
         pool = UnstoppableLender(poolAddress);
         owner = msg.sender;
     }
 
-    // Pool will call this function during the flash loan
+    /**
+        把借來的 token 還給 pool合約
+        pool合約的 flashLoan() 執行到一半時會呼叫此函式進行還款
+     */
     function receiveTokens(address tokenAddress, uint256 amount) external {
         require(msg.sender == address(pool), "Sender must be pool");
         // Return all tokens to the pool
         require(IERC20(tokenAddress).transfer(msg.sender, amount), "Transfer of tokens failed");
     }
 
+    // 用戶要借閃電貸時，呼叫此函式。 此函式再去呼叫 pool合約 的 flashLoan() 函式
     function executeFlashLoan(uint256 amount) external {
         require(msg.sender == owner, "Only owner can execute flash loan");
         pool.flashLoan(amount);
